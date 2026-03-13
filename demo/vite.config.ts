eeb3ef6ebc208601e7c66a4ed05d97f944230d9f
Duc Trung Le <leductrungxf@gmail.com>
2026-03-13 23:45:10 +0100

    chore: demo page

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/plainb/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
